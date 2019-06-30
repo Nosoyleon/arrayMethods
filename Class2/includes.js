922a32f99d57d197f12ac968c4f1c9dd60301850
León Ceballos <leon.ceballos@wolox.co>
2019-06-30 10:18:46 -0500

    Includes clarification

--- Class2/includes.js
+++ Class2/includes.js
@@ -1,8 +1,9 @@
 var mentors = ["Daniel", "Irini", "Ashleigh", "Rob", "Etzali"];
 
-function isAMentor(name) {
-  return mentors.includes(name);
+function isAMentor(name, from) {
+  return mentors.includes(name, from);
 }
 
-console.log("Is Rukmuni a mentor?");
-console.log(isAMentor("Rukmini"));
+console.log("Is Daniel a mentor?");
+console.log(isAMentor("Daniel", 0));
+console.log(isAMentor("Daniel", 1));
