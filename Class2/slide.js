5708f878d1c4595fbeb6c0787c42fb0122e7e781
León Ceballos <leon.ceballos@wolox.co>
2019-06-30 09:31:37 -0500

    Slide behaviour

--- Class2/slide.js
+++ Class2/slide.js
@@ -1,3 +1,28 @@
-var mentors = ["Daniel", "Irini", "Ashleigh", "Rob", "Etzali"];
+// Check with Primtive types
+var teaching = { class: "Javascript" };
 
-console.log("slide", mentors.slice(0, 2));
+var mentors = ["Daniel", "Irini", "Ashleigh", "Rob", "Etzali", teaching];
+
+// Reference
+
+var slicedMentors = mentors.slice(2, 3);
+console.log("sliced mentors", mentors);
+console.log("sliced mentors", slicedMentors);
+
+// Changing refrence
+
+var copiedMentors = mentors.slice();
+mentors[0] = "Daniel Mod";
+teaching.class = "Javascript 2";
+
+console.log("Mod");
+console.log(mentors);
+console.log(copiedMentors);
+
+// Examples
+console.log("default", mentors.slice()); //Result
+console.log("last?", mentors.slice(10)); //Result
+console.log("From 1 to penultimate", mentors.slice(1, -1));
+console.log("First 2", mentors.slice(0, 2));
+console.log("From 2", mentors.slice(2));
+console.log("Last 2", mentors.slice(-2));
